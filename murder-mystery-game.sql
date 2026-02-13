4e1958db1b2653389f8e037b6edf740164223c79
Priscilla Morrigan <24235887+RPMorrigan@users.noreply.github.com>
2026-02-13 15:50:05 -0700

    Document SQL detective process and insights
    Added comments detailing SQL queries and insights.

--- murder-mystery-game.sql
+++ murder-mystery-game.sql
@@ -1,7 +1,15 @@
 -- Record your SQL detective process here!  Write down: 
 --   1. The SQL queries you ran
+--        I mostly ran some pretty basic queries. Meaning, Select, from, where.
+--        I only really had to do run a LIKE query here and there. I would have had to run some stuff like
+--        between but the data was pretty specific so it didn't require much more than that.
+
 --   2. Any notes or insights as SQL comments
+--        I thankfully was pretty good about not mistaking the primary key for some of the entries for the wrong number
+--        in the other tables. I had someone ask me to help and I saw it was that in particular which was causing her issue.
+
 --   3. Your final conclusion: who did it?
+--      I'm pretty sure it's Jeremy Bowers
 
 -- Using these SQL clauses will help you solve the mystery: 
 --    SELECT, FROM, WHERE, AND, OR, ORDER BY, LIMIT, LIKE, DISTINCT, BETWEEN, AS
